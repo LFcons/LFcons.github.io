4a7bd0a1f2e806fc2f0fe8786fe718896c7b43d9
LFcons <luthermessking95@gmail.com>
2023-09-24 04:35:31 +0200

    Modification du script php: sendmemail 7

---
+++ assets/js/submit.js
@@ -0,0 +1,39 @@
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.querySelector("input[name='name']").value;
+    const email = document.querySelector("input[name='email']").value;
+    const message = document.querySelector("textarea[name='message']").value;
+
+    const secretLienduRepo = "${{ secrets.SECRET_URL_REQUETTE }}";
+    const secretTokenGitHub = "${{ secrets.SECRET_TOKTOK }}";
+
+    // Construire le corps de la requête JSON
+    const requestBody = {
+        name: name,
+        email: email,
+        message: message
+    };
+
+    // Effectuer une requête HTTP POST à l'API GitHub pour déclencher l'événement workflow_dispatch
+    fetch(secretLienduRepo, {
+        method: "POST",
+        headers: {
+            "Authorization": "token " + secretTokenGitHub,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            event_type: "submit_form",
+            client_payload: requestBody
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Réponse de l'API GitHub :", data);
+        alert("Workflow déclenché avec succès !");
+    })
+    .catch(error => {
+        console.error("Erreur lors de la soumission de la requête :", error);
+        alert("Une erreur s'est produite lors de la soumission du formulaire.");
+    });
+});
